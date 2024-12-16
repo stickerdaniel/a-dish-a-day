376d0592d835722424b30d01bb0d870ffbbbe479
Vincent Nahn <ProfessionalGriefer@users.noreply.github.com>
2024-12-16 15:23:49 +0100

    Initial Commit

---
+++ calendar/calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendarApp.swift
+//  calendar
+//
+//  Created by Vincent Nahn on 2024/12/16.
+//
+
+import SwiftUI
+
+@main
+struct calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
