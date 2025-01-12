71d57b85782f2bc0fa0fed616e7117969e8120f9
Lucy Plassmann <lucyplsm@users.noreply.github.com>
2025-01-12 11:57:23 +0100

    recipe view added

--- calendar/Views/Recipe/RecipeView.swift
+++ calendar/Views/Recipe/RecipeView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct RecipeView: View {
+    var recipes = Recipe.sampleRecipes
+    var columns = [GridItem(.adaptive(minimum: 100))]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationTitle("Recipes")
+        LazyVGrid(columns: columns) {
+            ForEach(recipes, id: \.self) { r in
+                RecipeGridItem(recipe: r)
+            }
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .padding(.top, 20)
+        .navigationTitle("Recipes")
+        
     }
-}
+    
+    struct RecipeGridItem: View {
+        var recipe: Recipe
+        var body: some View {
+            NavigationLink(destination: OpenRecipeView(recipe: recipe)) {
+                
+                Text(recipe.name)
+                    .padding()
+                    .frame(width: 100, height: 100)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 15.0)
+                            .stroke(.black, lineWidth: 2.0)
+                    )
+                }
+            }
+        }
+    }
+    
+    #Preview {
+        RecipeView()
+    }
 
-#Preview {
-    RecipeView()
-}
