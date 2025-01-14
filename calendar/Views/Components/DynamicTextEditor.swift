4a37ad50b8825a780759d561397b5381241dbc7c
Daniel Sticker <stickerdaniel@users.noreply.github.com>
2025-01-14 23:36:15 +0100

    feat: add recipe redesign

---
+++ calendar/Views/Components/DynamicTextEditor.swift
@@ -0,0 +1,53 @@
+//
+//  DynamicTextEditor.swift
+//  calendar
+//
+//  Created by Daniel Sticker on 14.01.25.
+//
+
+import SwiftUI
+
+struct DynamicTextEditor: View {
+    let placeholder: String
+    @Binding var text: String
+    @State private var textHeight: CGFloat = 40
+    var minHeight: CGFloat = 40 // Allow customization of default height
+
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(.gray)
+                    .padding(.leading, 4)
+                    .padding(.top, 8)
+            }
+
+            TextEditor(text: $text)
+                .frame(height: textHeight)
+        }
+        .onAppear {
+            updateHeight()
+        }
+        .onChange(of: text) {
+            updateHeight()
+        }
+    }
+
+    private func updateHeight() {
+        let textSize = text.heightWithConstrainedWidth(width: UIScreen.main.bounds.width - 40, font: UIFont.systemFont(ofSize: 17))
+        textHeight = max(minHeight, textSize + 20)
+    }
+}
+
+extension String {
+    func heightWithConstrainedWidth(width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(
+            with: constraintRect,
+            options: .usesLineFragmentOrigin,
+            attributes: [.font: font],
+            context: nil
+        )
+        return ceil(boundingBox.height)
+    }
+}
